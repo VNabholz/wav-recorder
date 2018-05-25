1e5b0eef11a264ffa960576a9e7fea30056552a1
vlinde <33723825+vlinde@users.noreply.github.com>
2018-05-25 11:52:48 +0300

    Update WAVRecorder.java

--- src/android/WAVRecorder.java
+++ src/android/WAVRecorder.java
@@ -7,12 +7,24 @@ import org.apache.cordova.media.FileHelper;
 import org.json.JSONArray;
 import org.json.JSONException;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.media.AudioFormat;
 import android.net.Uri;
 
 public class WAVRecorder extends CordovaPlugin {
 	public static String TAG = "WAVRecorder";
 	HashMap<String, ExtAudioRecorder> players;
+    private CallbackContext messageChannel;
+
+    public static String [] permissions = { Manifest.permission.RECORD_AUDIO, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+    public static int RECORD_AUDIO = 0;
+    public static int WRITE_EXTERNAL_STORAGE = 1;
+    public String identifier = "";
+    public int sampleRate ;
+    public int channels;
+    public int encoding;
+    public String target;
 
 	public WAVRecorder() {
 		this.players = new HashMap<String, ExtAudioRecorder>();
@@ -22,9 +34,10 @@ public class WAVRecorder extends CordovaPlugin {
 		CordovaResourceApi resourceApi = webView.getResourceApi();
 		PluginResult.Status status = PluginResult.Status.OK;
         String result = "";
+        messageChannel = callbackContext;
 
         if (action.equals("record")) {
-            this.startRecording(args.getString(0));
+            this.identifier = args.getString(0);
         }
         if (action.equals("recordForMillis")) {
             this.startRecording(args.getString(0), args.getInt(1));
@@ -38,19 +51,13 @@ public class WAVRecorder extends CordovaPlugin {
             return true;
         }
         else if (action.equals("create")) {
-            String id = args.getString(0);
-            String target = args.getString(1);
-            String fileUriStr;
-            try {
-                Uri targetUri = resourceApi.remapUri(Uri.parse(target));
-                fileUriStr = targetUri.toString();
-            } catch (IllegalArgumentException e) {
-                fileUriStr = target;
-            }
 
-            int sampleRate = args.getInt(2);
-            int channels = args.getInt(3);
-            int encoding = args.getInt(4);
+            identifier = args.getString(0);
+            target = args.getString(1);
+
+            sampleRate = args.getInt(2);
+            channels = args.getInt(3);
+            encoding = args.getInt(4);
 
             // Determine mono or stereo
             if (channels == 1) channels = AudioFormat.CHANNEL_IN_MONO;
@@ -60,17 +67,7 @@ public class WAVRecorder extends CordovaPlugin {
             if (encoding == 8) encoding = AudioFormat.ENCODING_PCM_8BIT;
             else encoding = AudioFormat.ENCODING_PCM_16BIT;
 
-            String src = FileHelper.stripFileProtocol(fileUriStr);
-            ExtAudioRecorder audio = ExtAudioRecorder.getInstance(
-            		this,
-            		id,
-            		sampleRate,
-            		channels,
-            		encoding
-            		);
-            audio.setOutputFile(src);
-
-            this.players.put(id, audio);
+            this.promptForRecord();
         }
         else if (action.equals("locate")) {
 
@@ -90,6 +87,7 @@ public class WAVRecorder extends CordovaPlugin {
     }
 
 	public void startRecording(String id) {
+
 		ExtAudioRecorder audio = this.players.get(id);
 		if (audio != null) {
 			audio.prepare();
@@ -140,6 +138,78 @@ public class WAVRecorder extends CordovaPlugin {
             audio.release();
         }
         this.players.clear();
+    }
+
+    private void promptForRecord()
+    {
+        if(PermissionHelper.hasPermission(this, permissions[WRITE_EXTERNAL_STORAGE])  &&
+                PermissionHelper.hasPermission(this, permissions[RECORD_AUDIO])) {
+
+            this.startRecording();
+        }
+        else if(PermissionHelper.hasPermission(this, permissions[RECORD_AUDIO]))
+        {
+            getWritePermission(WRITE_EXTERNAL_STORAGE);
+        }
+        else
+        {
+            getMicPermission(RECORD_AUDIO);
+        }
+
+    }
+
+    protected void getWritePermission(int requestCode)
+    {
+        PermissionHelper.requestPermission(this, requestCode, permissions[WRITE_EXTERNAL_STORAGE]);
+    }
+
+
+    protected void getMicPermission(int requestCode)
+    {
+        PermissionHelper.requestPermission(this, requestCode, permissions[RECORD_AUDIO]);
+    }
+
+    public void onRequestPermissionResult(int requestCode, String[] permissions,
+                                          int[] grantResults) throws JSONException
+    {
+        for(int r:grantResults)
+        {
+            if(r == PackageManager.PERMISSION_DENIED)
+            {
+
+                this.setState(ExtAudioRecorder.State.PERMISSION_DENIED);
+                return;
+            }
+        }
+        promptForRecord();
+    }
+
+    private void setState(ExtAudioRecorder.State state) {
+
+        String message = "Permission(s) denied.";
+        this.webView.sendJavascript("cordova.require('ro.martinescu.audio.WAVRecorder').onStatus('" + identifier + "', " + 1 + ", '" + state.toString() + "', '" + message + "' );");
+    }
+
+    private void startRecording() {
+        String fileUriStr;
+        try {
+            Uri targetUri = webView.getResourceApi().remapUri(Uri.parse(target));
+            fileUriStr = targetUri.toString();
+        } catch (IllegalArgumentException e) {
+            fileUriStr = target;
+        }
+
+        String src = FileHelper.stripFileProtocol(fileUriStr);
+        ExtAudioRecorder audio = ExtAudioRecorder.getInstance(
+                this,
+                identifier,
+                sampleRate,
+                channels,
+                encoding
+        );
+        audio.setOutputFile(src);
+        this.players.put(identifier, audio);
+        this.startRecording(identifier);
     }
 
 }
